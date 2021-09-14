848b15b6b5b4a78c065f35f7ab3f9f17b5ca3164
saidulislam <saidul.islam@gmail.com>
2021-09-14 16:29:16 -0400

    crud with sqlite

---
+++ python_sql_database/utils/DatabaseConnection.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, host: str):
+        self.connection = None
+        self.host = host
+
+    def __enter__(self) -> sqlite3.Connection:
+        self.connection = sqlite3.connect(self.host)
+        return self.connection
+
+    def __exit__(self, exc_type, exc_value, ec_tb):
+        self.connection.commit()
+        self.connection.close()
